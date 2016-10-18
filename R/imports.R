b2e0a82a7ec1520423cbd0cb49a37942aee6356a
Rob van Harrevelt <rvh@cpb.nl>
2016-10-18 10:48:22 +0200

    Improved the make system. The Dynamic Library can now be loaded in R.

--- R/imports.R
+++
@@ -1,1 +0,0 @@
-#' @importFrom Rcpp sourceCpp
--- demo/test_islm.R
+++ demo/test_islm.R
@@ -1,6 +1,6 @@
 library(dynparse)
 
-mod_file <- "islm.mod"
+mod_file <-"demo/islm.mod"
 mod_dir <- system.file("extdata", package = "dynparse")
 mod_file_org <- file.path(mod_dir, "islm.mod")
 file.copy(mod_file_org, mod_file)
