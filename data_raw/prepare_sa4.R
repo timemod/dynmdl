9a9b26c5ed149fc1b059e3c4cb40f29949291b1c
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2016-11-01 11:14:49 +0100

    Removed islm and sa4 model from the Repositories. The examples are now moved to the directory rationele_verwachtingen.

--- data_raw/prepare_sa4.R
+++
@@ -1,6 +0,0 @@
-library(dynr)
-
-mod_file <- system.file("extdata", "sa4.mod", package = "dynr")
-print(system.time(sa4_mdl <- compile_model(mod_file)))
-
-devtools::use_data(sa4_mdl, overwrite = TRUE)
