5e13b70d37a03d001807ee60c97daa2685d61edf
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2017-01-19 17:47:22 +0100

    Problems for Windows corrected.

--- pkg/R/compile_c_functions.R
+++ pkg/R/compile_c_functions.R
@@ -2,9 +2,10 @@ compile_c_functions <- function(dll_dir) {
 
     cat("Compiling C functions ...\n")
 
+    is_windows <- .Platform$OS.type == "windows"
+
     mdl_function_file <- system.file("c_wrappers", "mdl_functions.c",
                                      package = "dynr")
-    printobj(mdl_function_file)
 
     file.copy(mdl_function_file, file.path(dll_dir, "mdl_functions.c"),
               overwrite = TRUE)
@@ -13,11 +14,17 @@ compile_c_functions <- function(dll_dir) {
     R <- file.path(r_home, "R")
     CC <- system("R CMD config CC", intern = TRUE)
 
+    if (is_windows) {
+        cflags <- "-c"
+    } else {
+        cflags <- "-c -fPIC"
+    }
+
     # separate compilation of f_static and f_dynamic (because we do not want
     # to use optimalization)
-    system(paste(CC, "-c -fPIC", file.path(dll_dir, "f_static.c"),
+    system(paste(CC, cflags, file.path(dll_dir, "f_static.c"),
              "-o", file.path(dll_dir, "f_static.o")))
-    system(paste(CC, "-c -fPIC", file.path(dll_dir, "f_dynamic.c"),
+    system(paste(CC, cflags, file.path(dll_dir, "f_dynamic.c"),
              "-o", file.path(dll_dir, "f_dynamic.o")))
 
     source_file <- paste(dll_dir, c("mdl_functions.c", "f_static.c",
@@ -30,5 +37,11 @@ compile_c_functions <- function(dll_dir) {
     cat(output)
     cat("Done\n")
 
-    dyn.load(file.path(dll_dir, "mdl_functions.so"))
+
+    if (is_windows) {
+        dll_ext <- "dll"
+    } else {
+        dll_ext <- "so"
+    }
+    dyn.load(file.path(dll_dir, paste("mdl_functions", dll_ext, sep = ".")))
 }
