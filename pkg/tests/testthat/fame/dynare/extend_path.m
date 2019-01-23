589cf52f6ffd677a4f7390d54c661693cf2ca9c0
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2019-01-23 15:42:34 +0100

    The dynare output in test directory fame has now also been created with Matlab on Windows with Dynare 4.5.6.

--- pkg/tests/testthat/fame/dynare/extend_path.m
+++ pkg/tests/testthat/fame/dynare/extend_path.m
@@ -1,5 +1,5 @@
 function extend_path()
     addpath utils
     if findstr(getenv('OS'), 'Windows_NT')
-        addpath m:\a_io\private\rvh\matlab\dynare\4.4.3\matlab
+        addpath m:\p_dynare\dynare\4.5.6_optim_only\matlab
     end
