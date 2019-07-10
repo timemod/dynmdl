f44305beedaa7cc0c0880b3dd836c9de4abb400e
Harrevelt <rvh@rene-thinkpad.cpb.nl>
2019-07-10 17:40:58 +0200

    Corrected typo: resisuals replaced by redisuals.

--- pkg/R/create_fit_mod.R
+++ pkg/R/create_fit_mod.R
@@ -100,7 +100,7 @@ convert_mod <- function(input_file, output_file, fit_cond) {
       model_found <- TRUE
     } else if (in_model && endsWith(trimws(line, "right"), "end;")) {
       writeLines(gsub("end;", "", line), con = output)
-      writeLines(c("% First order condition the resisuals:", ""),
+      writeLines(c("% First order conditions residuals:", ""),
                  con = output)
       writeLines(strwrap(fit_cond$res_equations, width = 80),
                  con = output)
