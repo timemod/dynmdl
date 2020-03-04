5b2c7e3df9653c2f05f7c52e63a5fb5e334081ac
Rob van Harrevelt <rvanharrevelt@gmail.com>
2020-03-04 10:49:20 +0100

    Added a test for update_endos.

--- pkg/tests/testthat/islm/test_run_initval.R
+++ pkg/tests/testthat/islm/test_run_initval.R
@@ -32,18 +32,26 @@ test_that("run_initval for model with fit", {
   
   # first test: only modify normal endogenous and exogenous variables
   mdl2 <- mdl_fit$copy()
-  mdl2$set_static_data(c(g = 100, c = 999, yd = 5555,  t = 111))
   mdl2$run_initval()
   expect_equal(mdl2$get_static_data(), initval_data_fit_correct)
   
   # run_initval does not modify variables that do not occur in the 
   # left hand side of an equation of the initval block. 
   # There are not set to zero.
-  mdl2$set_static_data(c(ui = 100))
+  mdl2$set_static_data(c(g = 100, c = 999, yd = 5555,  t = 111, ui = 100))
   mdl2$run_initval()
   expect_equal(mdl2$get_static_data(names = "ui"), c(ui = 100))
   expect_equal(mdl2$get_static_endos(), 
                initval_data_fit_correct[mdl2$get_endo_names()])
+  expect_equal(mdl2$get_static_exos(), 
+               initval_data_fit_correct[mdl2$get_exo_names()])
+
+  # nu met update_endos = false
+  mdl2$set_static_data(c(g = 100, c = 999, yd = 5555,  t = 111, ui = 100))
+  new_static_endos <- mdl2$get_static_endos()
+  mdl2$run_initval(update_endos = FALSE)
+  expect_equal(mdl2$get_static_data(names = "ui"), c(ui = 100))
+  expect_equal(mdl2$get_static_endos(), new_static_endos)
   expect_equal(mdl2$get_static_exos(), 
                initval_data_fit_correct[mdl2$get_exo_names()])
 })
