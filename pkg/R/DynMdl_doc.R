7bb4aab127bc33020dc501342e08b773dffab6e1
Rob van Harrevelt <rvanharrevelt@gmail.com>
2020-03-04 10:42:20 +0100

    Added tests for run_initval.

--- pkg/R/DynMdl_doc.R
+++ pkg/R/DynMdl_doc.R
@@ -1552,7 +1552,11 @@ NULL
 #' @description
 #' 
 #' This function runs the equations in the initval block using the 
-#' current values of parameters and static model varobales.
+#' current values of parameters and static model variables.
+#' 
+#' Variables that do not occur at the left hand side of an equation in the
+#' initval block are *not* modified. Thus they are not initialized to 0, in
+#' contrast to the evaluation of the initval block by the Dynare parser.
 #' 
 #' @section Usage:
 #' \code{DynMdl} method:
