66b5c8557b32a66505d57ca35aa0bcc2f1b11ffd
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2024-09-24 15:03:06 +0200

    Error in tools corrected.

--- tools/gendepend.R
+++ tools/gendepend.R
@@ -3,6 +3,10 @@
 # directory pkg/src on the include files in pkg/src and pkg/src/macro.
 # A dependency list is written file deps/deps.rds.
 
+if (!require(igraph)) {
+  install.packages("igraph", repos = "https://cloud.r-project.org")
+}
+
 library(igraph)
 library(stringr)
 library(tictoc)
@@ -26,7 +30,7 @@ read_includes <- function(filename, src_dir, is_macro_dir = FALSE) {
     }
   }
   if (length(includes) == 0) {
-    return(NULL) 
+    return(NULL)
   } else {
     return(includes)
   }
@@ -40,18 +44,20 @@ get_dep_list <- function(filenames, src_dir, is_macro_dir = FALSE) {
   return(retval)
 }
 
-update_deps <- function(deps, filenames, src_dir) {
-  
+update_deps <- function(deps, filenames, src_dir, header_ext) {
+
   is_macro_file <- grepl("^macro/", filenames)
-  
+
   macro_files <- filenames[is_macro_file]
   other_files <- filenames[!is_macro_file]
-  
+
   # for macro files we only need header files
-  header_pattern <- paste0("\\.(",  paste(header_ext, collapse = "|"),
-                                          ")$")
+  header_pattern <- paste0("\\.(",  paste(header_ext, collapse = "|"), ")$")
+  print(header_pattern)
   macro_files <- grep(header_pattern, macro_files, value = TRUE)
- 
+  cat("\nmacro_files\n")
+  print(macro_files)
+
   if (length(macro_files) > 0) {
     macro_deps <- get_dep_list(macro_files, src_dir = src_dir,
                                is_macro_dir = TRUE)
@@ -62,47 +68,48 @@ update_deps <- function(deps, filenames, src_dir) {
                                is_macro_dir = FALSE)
     deps[names(other_deps)] <- other_deps
   }
-  
-  # remove unnessary dependencies 
+
+  # remove unnessary dependencies
   is_null <- sapply(deps, FUN = is.null)
   return(deps[!is_null])
 }
 
 if (interactive() || !file.exists(dep_rds)) {
-  
+
   #
   # This script is run from RStudio or the dep_rds file
   # does not exist.
   #
-  
-  pattern <- paste0("\\.(",  
+
+  pattern <- paste0("\\.(",
                     paste(c(src_ext, header_ext), collapse = "|"),
                     ")$")
   filenames <- list.files(src_dir, pattern = pattern, recursive = TRUE)
-  
+
   cat("\nAnalyzing dependencies of c++ files on header files\n\n")
-  
+
   deps <- list()
-  
+
 } else {
-  
+
   # This script is called from the makefile Makedeps,
   # The command line arguments are the names of the files
   # that are newer than the dep_rds file.
-  
+
   filenames <- commandArgs(trailingOnly = TRUE)
   pattern <- paste0("^", src_dir, "/")
   pattern <- sub("/", "(/|\\\\\\\\)", pattern)
   filenames <- sub(pattern, "", filenames)
-  
+
   cat("\nUpdating dependencies of C++ files on header files:\n",
       paste(filenames, collapse = "\n"), "\n\n")
-  
+
   deps <- readRDS(dep_rds)
 }
 
 tic("Analyzing dependencies")
-deps <- update_deps(deps, filenames, src_dir = src_dir)
+deps <- update_deps(deps, filenames, src_dir = src_dir,
+                    header_ext = header_ext)
 toc()
 
 saveRDS(deps, dep_rds)
