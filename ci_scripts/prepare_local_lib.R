3daa065399fa8f375a9429cf6928f4777ce3af88
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2026-06-26 15:21:37 +0200

    Simplified install_deps.R.

--- ci_scripts/prepare_local_lib.R
+++ ci_scripts/prepare_local_lib.R
@@ -1,29 +1,6 @@
-# Set up Repositories: CPB and CRAN
-repos <- c(
-  "https://cloud.r-project.org",           # CRAN
-  "file:/usr/local/lib/R/cpblib/cpb_repo"  # CPB
-)
-
-# If the user library does not exist, then create it
-# and add it to the library path.
-user_lib_dir <- Sys.getenv("R_LIBS_USER")
-if (!dir.exists(user_lib_dir)) {
-  dir.create(user_lib_dir, recursive = TRUE)
-  .libPaths(user_lib_dir)
-}
-
-cat("\nLibrary paths:\n")
-print(.libPaths())
-
-if (!require(devtools, quietly = TRUE)) {
-  install.packages("devtools", repos = repos)
-}
-if (!require(lintr, quietly = TRUE)) {
-  install.packages("lintr", repos = repos)
-}
-
-# install and update all dependenencies
-devtools::install_deps("pkg", dependencies = TRUE, repos = repos)
+source("install_deps.R")
 
 # make sure all packages (including devtools and lintr) are up-to-date.
-update.packages(lib.loc = user_lib_dir, repos = repos, ask = FALSE)
+user_lib_dir <- Sys.getenv("R_LIBS_USER")
+update.packages(lib.loc = user_lib_dir, repos = repos, 
+                ask = FALSE) 
--- install_deps.R
+++ install_deps.R
@@ -1,13 +1,38 @@
-#!/usr/bin/Rscript
-repo <- "https://cloud.r-project.org"
 
-# install extra packages needed to install isismdl with the install script.
-extra_packages <- c("devtools", "tictoc", "igraph")
+#!/usr/bin/Rscript
+
+# Set up Repositories: CPB and CRAN
+repos <- c(
+  CRAN = "https://cloud.r-project.org",
+  CPB = "http://gitlab.cpb.nl:8880/io/cpb_repo/"
+)
+
+options(repos = repos)
+
+user_lib_dir <- Sys.getenv("R_LIBS_USER")
+if (!dir.exists(user_lib_dir)) {
+  dir.create(user_lib_dir, recursive = TRUE)
+}
+
+cat("library-pad:\n")
+print(.libPaths())
+
+# Install 'pak' if needed
+if (!require("pak", character.only = TRUE, quietly = TRUE)) {
+  install.packages("pak", dependencies = FALSE)
+} else {
+  unloadNamespace("pak")
+}
+
+# Install extra packages
+extra_packages <- c("devtools", "igraph", "lintr")
 for (extra_package in extra_packages) {
   if (!require(extra_package, character.only = TRUE, quietly = TRUE)) {
-    install.packages(extra_package, repos = repo)
+    pak::pkg_install(pkg, lib = .libPaths(), upgrade = FALSE)
+  } else {
+    unloadNamespace(extra_package)
   }
 }
 
-devtools::install_deps("pkg", dependencies = TRUE, upgrade = "never",
-                       repos = repo)
+# Install and update dependencies
+pak::local_install_dev_deps("pkg", lib = .libPaths(), upgrade = FALSE)
